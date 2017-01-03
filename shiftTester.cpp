c864366257c200a3687554a38c229989fb405e87
asya-nastya <asya.zbenyakova@gmail.com>
2017-01-03 21:06:00 +0200

    testProgram

---
+++ shiftTester.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <math.h> 
+#include <emmintrin.h> 
+
+using namespace std; 
+
+int main () {
+	int a = 3;
+	int shiftA = a << 1;
+	cout << shiftA;
+	
+	
+	int one = 3;
+	int two = 4;
+	int sum = 3|4;
+	cout <<sum;
+}
